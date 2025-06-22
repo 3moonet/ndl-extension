77eff620f833e946bf0774215cb91a33aa67164b
3moonet <sanmoon429@gmail.com>
2025-06-22 17:57:02 +0900

    書籍情報をフォーマットする関数を新規作成

---
+++ src/content/BookInfo/format.ts
@@ -0,0 +1,24 @@
+import type { BookInfo } from "./types";
+
+export const formatBookInfo = ({
+  authors,
+  title,
+  volume,
+  seriesTitle,
+  publisher,
+  issued,
+  ndlCallNumber,
+}: BookInfo): string => {
+  return (
+    `${authors}，` +
+    `『${title}』${volume}，` +
+    formatSeriesTitle(seriesTitle) +
+    `${publisher}，` +
+    `${issued}，` +
+    `【${ndlCallNumber}】`
+  );
+};
+
+const formatSeriesTitle = (series_title: string): string => {
+  return series_title ? `（東洋文庫293${series_title}），` : "";
+};
