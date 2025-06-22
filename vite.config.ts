3a625d9b91c022ee1eae37adcdd128d26395f60d
3moonet <sanmoon429@gmail.com>
2025-06-22 15:03:18 +0900

    ndl用に簡易セットアップ

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { crx } from "@crxjs/vite-plugin";
-import manifest from "./manifest.json";
+import { crx, defineManifest } from "@crxjs/vite-plugin";
+
+const manifest = defineManifest({
+  name: "NDL Extension",
+  description: "NDL Extension",
+  version: "1.0.0",
+  manifest_version: 3,
+  content_scripts: [
+    {
+      matches: ["https://ndlsearch.ndl.go.jp/books/*"],
+      js: ["src/content/index.tsx"],
+    },
+  ],
+});
 
 // https://vite.dev/config/
 export default defineConfig({
